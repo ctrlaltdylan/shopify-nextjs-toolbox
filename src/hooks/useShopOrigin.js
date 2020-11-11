90cac26297174acaf058de3e2f5cd98f5dd59cb9
Dylan Pierce <me@dylanjpierce.com>
2020-11-11 10:14:08 -0500

    Moving to hooks

--- src/hooks/useShopOrigin.js
+++ src/hooks/useShopOrigin.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 /**
  * Retrieve the shopOrigin query param and store it locally for future use
  */
-export default function getShopOrigin() {
+export default function useShopOrigin() {
   const [shopOrigin, setShopOrigin] = useState(false);
   useEffect(() => {
     if (typeof window !== "undefined" && window.location) {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export * from "./middleware";
 export { default as useApi } from "./hooks/useApi";
 export { default as SessionProvider } from "./components/SessionProvider";
-export { default as getShopOrigin } from "./helpers/getShopOrigin";
+export { default as getShopOrigin } from "./hooks/getShopOrigin";
+export { default as useShopOrigin } from "./hooks/useShopOrigin";
 export { default as SessionTokenContext } from './contexts/SessionTokenContext';

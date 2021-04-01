65cfdbc70faa08301a78951fc8466f8cb14bbab7
Dylan Pierce <me@dylanjpierce.com>
2021-04-01 17:37:42 -0400

    Adding support for host as well as absolutel URL redirets

--- src/middleware/oauth/handleAuthCallback.js
+++ src/middleware/oauth/handleAuthCallback.js
@@ -1,6 +1,7 @@
 import querystring from "querystring";
 import verifyHmac from "./verifyHmac";
 import exchangeAccessToken from './exchangeAccessToken';
+import { URL } from 'url'
 
 export default handler => {
   return async (req, res) => {
@@ -19,11 +20,27 @@ export default handler => {
 
     try {
       const accessToken = await exchangeAccessToken(req.query.shop, accessTokenQuery);
+      // optional redirectUrl from the user, by default we use HOME_PATH
+      const redirect = await handler(req, res, accessToken);
 
-      const redirectUrl = await handler(req, res, accessToken);
+      if(!redirect) {
+        res.redirect(`${process.env.HOME_PATH}?${querystring.stringify(req.query)}`);
+        return;
+      }
+      
+      // if redirectUrl.origin is null, it means we have been given a path, not a fully qualified URL
+      const redirectUrl = new URL(redirect);
+      const redirectUrlQuery = Object.fromEntries(redirectUrl.searchParams);
 
-      // finished with oauth! Redirect to home page or the custom URL provided by the handler
-      res.redirect(`${redirectUrl || process.env.HOME_PATH}?shop=${req.query.shop}`);
+      if(redirectUrl.origin) {
+        res.redirect(
+          `${redirectUrl.origin}${redirectUrl.pathname}?${querystring.stringify({...req.query, ...redirectUrlQuery})}`
+        );
+        return;
+      }
+
+      throw new Error(`Please provide a fully qualifed URL. Instead received ${redirect}`);
+
     } catch (err) {
       console.log(err);
       res.status(401).json({ message: "Error while retrieving access token.", error: err });

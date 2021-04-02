7aa0a37e93e34c637b3a9f63456c7fd62804bf1d
Dylan Pierce <me@dylanjpierce.com>
2021-04-02 07:45:22 -0400

    Adding host support

--- src/hooks/useHost.js
+++ src/hooks/useHost.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
  * Retrieve the host query param and store it locally for future use
  */
 export default function useHost() {
-  const [host, sethost] = useState(false);
+  const [host, setHost] = useState(false);
   useEffect(() => {
     if (typeof window !== "undefined" && window.location) {
       const queryHost = new URLSearchParams(window.location.search).get(
--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ export { default as useApi } from "./hooks/useApi";
 export { default as SessionProvider } from "./components/SessionProvider";
 export { default as getShopOrigin } from "./hooks/getShopOrigin";
 export { default as useShopOrigin } from "./hooks/useShopOrigin";
+export { default as useHost } from "./hooks/useHost";
 export { default as SessionTokenContext } from './contexts/SessionTokenContext';

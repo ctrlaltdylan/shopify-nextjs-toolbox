e492ab70f5d43af8bc23a4fc377c5b4368dfc8d5
ezerway <live.ezerway@gmail.com>
2021-09-11 02:38:07 -0700

    Bugfix: Sometimes app is null on useApi hook

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import axios from "axios";
 import { getSessionToken } from "@shopify/app-bridge-utils";
 import { Context as ShopifyAppContext } from "@shopify/app-bridge-react";
@@ -12,16 +12,21 @@ export default function useApi() {
   const api = axios.create();
   const app = useContext(ShopifyAppContext);
 
-  api.interceptors.request.use((config) => {
-    return getSessionToken(app)
-      .then((token) => {
-        config.headers["Authorization"] = `Bearer ${token}`;
-        return config;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+  useEffect(() => {
+    if (!app) {
+      return;
+    }
+    api.interceptors.request.use((config) => {
+      return getSessionToken(app)
+        .then((token) => {
+          config.headers["Authorization"] = `Bearer ${token}`;
+          return config;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+  }, [app])
 
   return api;
 }
